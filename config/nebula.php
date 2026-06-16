107ee2cf1ca2dc202a499f7d008ddc7c19e865af
sonarta <firdausriawan2@gmail.com>
2026-06-16 14:30:53 +0700

    chore(config): bump version to 1.7.0

--- config/nebula.php
+++ config/nebula.php
@@ -11,7 +11,7 @@ return [
     | to compare against the latest GitHub release.
     |
     */
-    'version' => '1.6.3',
+    'version' => '1.7.0',
 
     /*
     |--------------------------------------------------------------------------
